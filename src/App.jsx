085c2d1f2a02e2d7d367aff9c8fe46cbf8c3f62b
yago14 <ggbravier@gmail.com>
2023-12-28 23:57:48 -0300

    configurando base vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { Home } from "./pages/Home"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Details } from "./pages/details/Details"
-import { ThemeProvider } from "./contexts/theme-contex"
-import { ThemeTogglerButton } from "./components/theme-toggler-button/Toggler-button"
+
 
 function App() {
 
   
 
   return (
-    <div >
+    <>
     
      
       <BrowserRouter>
@@ -20,7 +19,7 @@ function App() {
       </BrowserRouter>
      
 
-    </div>
+    </>
   )
 }
 
--- src/components/GetPokemon.jsx
+++ src/components/GetPokemon.jsx
@@ -1,5 +1,5 @@
 
-import React,{ useState, useEffect, useContext } from "react"
+import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 
